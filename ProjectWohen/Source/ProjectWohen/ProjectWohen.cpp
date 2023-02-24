2baf04455d18c790fb89de68160fafdf5938f7f4
Robyn <92083124+AleneVedMidnat@users.noreply.github.com>
2023-02-24 14:32:51 +0000

    initial

---
+++ ProjectWohen/Source/ProjectWohen/ProjectWohen.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectWohen.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectWohen, "ProjectWohen" );
+
+DEFINE_LOG_CATEGORY(LogProjectWohen)
+ 
---
+++ ProjectWohen/Source/ProjectWohen/ProjectWohenGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectWohenGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectWohenGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectWohenGameMode();
+};
+
+
+
