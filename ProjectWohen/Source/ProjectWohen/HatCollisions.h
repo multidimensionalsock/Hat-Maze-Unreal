5f3b7e6237ede89beb9a461dad85b83557e13570
Robyn <92083124+AleneVedMidnat@users.noreply.github.com>
2023-03-26 15:29:30 +0100

    hat collisions

---
+++ ProjectWohen/Source/ProjectWohen/HatCollisions.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HatCollisions.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PROJECTWOHEN_API UHatCollisions : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UHatCollisions();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(VisibleDefaultsOnly, Category="Collision")
+	class UBoxComponent* boxCollision;
+
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* overlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	
+
+		
+};
